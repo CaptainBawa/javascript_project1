1f961daaa2239033b48fa9f438a50ac07f9635ca
Collins Bawa <collinskwamebawa@yahoo.com>
2022-08-17 12:09:29 +0000

    game function added and few errors fixed

--- javascript.js
+++ javascript.js
@@ -1,12 +1,7 @@
-console.log('Hello World!')
-
-/* The above code is asking the user to input a string of either rock, paper or scissors and store it in userInput variable. */
-//let userInput = prompt(/Enter Rock, Paper or Scissors/gi)
-
 
 /* Creating a function that will randomly select one of the three options. */
 const getComputerChoice = () => {
-    const computer = ['Rock', 'Paper', 'Scissors'] /* Creating an array of items that the computer will make choice from. */
+    const computer = ['rock', 'paper', 'scissors'] /* Creating an array of items that the computer will make choice from. */
     return computer[Math.floor(Math.random() * computer.length)] /* Returning a random value from the array computer. */
 }
 
@@ -23,20 +18,31 @@ const playRound = (playerSelection, computerSelection) => {
    if(playerSelection === computerSelection){
     return `It's a tie! Both picked ${playerSelection}`
    }
-    else if(playerSelection === 'Rock' && computerSelection === 'Scissors'){
+    else if(playerSelection === 'rock' && computerSelection === 'scissors'){
     return 'You Win! Rock beats Paper'
    }
-    else if(playerSelection === 'Paper' && computerSelection === 'Rock'){
+    else if(playerSelection === 'paper' && computerSelection === 'rock'){
     return 'You Win! Paper beats Scissors'
    }
-    else if(playerSelection === 'Scissors' && computerSelection === 'Paper'){
+    else if(playerSelection === 'scissors' && computerSelection === 'paper'){
     return 'You Win! Scissors beats Rock'
    }
     else {
     return `You Lose! ${computerSelection} beats ${playerSelection}`
-   }
-   
+   } 
 }
-const playerSelection = 'Rock'
-const computerSelection = getComputerChoice()
-console.log(playRound(playerSelection, computerSelection))
+/* This is prompting the user to enter a value and then converting it to lowercase. */
+let playerSelection = prompt('Enter Rock, Paper or Scissors').toLowerCase()
+let computerSelection = getComputerChoice()
+
+
+/**
+ * The game function loops through the playRound function 5 times, and prints the results of each round
+ * to the console
+ */
+const game = () => {
+   for(let i = 1; i <= 5; i++){
+    console.log(playRound(playerSelection, computerSelection))
+   }
+}
+console.log(game())
